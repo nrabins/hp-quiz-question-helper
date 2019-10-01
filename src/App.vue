d8ddedb925a7b7238e05aa9ba998a34b2796860e
Nate Rabins <nrabins@alkamitech.com>
2019-10-01 13:23:06 -0500

    Work in progress

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App!!"/>
+    <v-input/>
+    <v-output/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Input from '@/components/Input';
+import Output from '@/components/Output';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    'v-input': Input,
+    'v-output': Output
   }
 }
 </script>
---
+++ src/components/Input.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div>
+      <input type="text" placeholder="Question"/>
+    </div>
+    Input
+  </div>
+</template>
